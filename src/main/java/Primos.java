e3e223e62c35a7bb1c848e7a7ef65ca2980bab7d
Walcker Gomes <walckergomes@gmail.com>
2022-04-11 14:55:54 -0300

    Rodando

--- src/main/java/Primos.java
+++ src/main/java/Primos.java
@@ -7,14 +7,21 @@ public class Primos {
         }
         int qnt = Integer.parseInt(args[0]); //Converte para número a entrada
         //TODO imprimir N primos
-        for (int i = 1; i <= qnt; i++) {
+        int aux2 = 0;
+        for (int i = 1; ; i++) {
             int aux = 0;
-            for (int j = 1; j <= i; j++) {
+            for (int j = 1; j <= i; j++) { // Um for dentro de outo para dividir todos os numeros
+                // até o escolhido pelo argumento
                 if (i % j == 0) ++aux; //Incrementa a variável AUX caso encontre uma divisão sem resto.
             }
             if (aux == 2) {  //Se no final AUX for igual a 2 é porque o número foi dividido por 1 e ele mesmo,
                 // portanto é primo.
                 System.out.println(i);
+                ++aux2;
+
+                }
+            if(aux2 == qnt){
+                break;
             }
         }
     }
